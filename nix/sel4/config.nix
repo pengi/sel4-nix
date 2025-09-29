9c8f8a18d068c7618240bd1bb4d2db04d6ca3dd2
Max Sikström <max@pengi.se>
2025-09-29 20:43:04 +0200

    nix - bundle sel4 kernel in a directory

---
+++ nix/sel4/config.nix
@@ -0,0 +1,12 @@
+{
+  "AARCH64_bcm2711_verified" = {
+    bintools = nixpkgs.pkgsCross.aarch64-embedded.stdenv.cc.bintools;
+    cc = nixpkgs.pkgsCross.aarch64-embedded.stdenv.cc;
+    toolchain = "aarch64-none-elf-";
+  };
+  "X64_verified" = {
+    bintools = nixpkgs.stdenv.cc.bintools;
+    cc = nixpkgs.stdenv.cc;
+    toolchain = "";
+  };
+}
--- nix/sel4/default.nix
+++ nix/sel4/default.nix
@@ -4,20 +4,8 @@
   config ? "AARCH64_bcm2711_verified",
 }:
 let
-  args =
-    {
-      "AARCH64_bcm2711_verified" = {
-        bintools = nixpkgs.pkgsCross.aarch64-embedded.stdenv.cc.bintools;
-        cc = nixpkgs.pkgsCross.aarch64-embedded.stdenv.cc;
-        toolchain = "aarch64-none-elf-";
-      };
-      "X64_verified" = {
-        bintools = nixpkgs.stdenv.cc.bintools;
-        cc = nixpkgs.stdenv.cc;
-        toolchain = "";
-      };
-    }
-    .${config};
+  board_config = import ./config.nix;
+  args = board_config.${config};
 in
 nixpkgs.stdenvNoCC.mkDerivation {
   name = "sel4-kernel-${config}";
