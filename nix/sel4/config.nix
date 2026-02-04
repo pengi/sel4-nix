1f5f659938743d5e1d813f0259197ac28505510b
Max Sikström <max@pengi.se>
2026-02-04 07:23:04 +0000

    Build seL4 14.0.0, and for all targets
    Changes the interface to be a dict of targets rather than
    parameterized build. Makes it easier to test build all targets

--- nix/sel4/config.nix
+++ nix/sel4/config.nix
@@ -1,13 +1,59 @@
 { nixpkgs }:
-{
-  "AARCH64_bcm2711_verified" = {
+let
+  toolchain_aarch64 = {
     bintools = nixpkgs.pkgsCross.aarch64-embedded.stdenv.cc.bintools;
     cc = nixpkgs.pkgsCross.aarch64-embedded.stdenv.cc;
     toolchain = "aarch64-none-elf-";
   };
-  "X64_verified" = {
-    bintools = nixpkgs.stdenv.cc.bintools;
-    cc = nixpkgs.stdenv.cc;
-    toolchain = "";
+  toolchain_armhf = {
+    bintools = nixpkgs.pkgsCross.armhf-embedded.stdenv.cc.bintools;
+    cc = nixpkgs.pkgsCross.armhf-embedded.stdenv.cc;
+    toolchain = "arm-none-eabihf-";
   };
+  toolchain_riscv64 = {
+    bintools = nixpkgs.pkgsCross.riscv64-embedded.stdenv.cc.bintools;
+    cc = nixpkgs.pkgsCross.riscv64-embedded.stdenv.cc;
+    toolchain = "riscv64-none-elf-";
+  };
+  toolchain_x86_64 = {
+    bintools = nixpkgs.pkgsCross.x86_64-embedded.stdenv.cc.bintools;
+    cc = nixpkgs.pkgsCross.x86_64-embedded.stdenv.cc;
+    toolchain = "x86_64-elf-";
+  };
+in
+{
+  "AARCH64_bcm2711_verified" = toolchain_aarch64;
+  "AARCH64_hikey_verified" = toolchain_aarch64;
+  "AARCH64_imx8mm_verified" = toolchain_aarch64;
+  "AARCH64_imx8mq_verified" = toolchain_aarch64;
+  "AARCH64_imx93_verified" = toolchain_aarch64;
+  "AARCH64_maaxboard_verified" = toolchain_aarch64;
+  "AARCH64_odroidc2_verified" = toolchain_aarch64;
+  "AARCH64_odroidc4_verified" = toolchain_aarch64;
+  "AARCH64_rockpro64_verified" = toolchain_aarch64;
+  "AARCH64_tqma_verified" = toolchain_aarch64;
+  "AARCH64_tx1_verified" = toolchain_aarch64;
+  "AARCH64_ultra96v2_verified" = toolchain_aarch64;
+  "AARCH64_verified" = toolchain_aarch64;
+  "AARCH64_zynqmp_verified" = toolchain_aarch64;
+  "ARM_am335x_verified" = toolchain_armhf;
+  "ARM_bcm2837_verified" = toolchain_armhf;
+  "ARM_exynos4_verified" = toolchain_armhf;
+  "ARM_exynos5410_verified" = toolchain_armhf;
+  "ARM_exynos5422_verified" = toolchain_armhf;
+  "ARM_hikey_verified" = toolchain_armhf;
+  "ARM_HYP_exynos5_verified" = toolchain_armhf;
+  "ARM_HYP_exynos5410_verified" = toolchain_armhf;
+  "ARM_HYP_verified" = toolchain_armhf;
+  "ARM_imx8mm_verified" = toolchain_armhf;
+  "ARM_MCS_verified" = toolchain_armhf;
+  "ARM_omap3_verified" = toolchain_armhf;
+  "ARM_tk1_verified" = toolchain_armhf;
+  "ARM_verified" = toolchain_armhf;
+  "ARM_zynq7000_verified" = toolchain_armhf;
+  "ARM_zynqmp_verified" = toolchain_armhf;
+  "RISCV64_MCS_verified" = toolchain_riscv64;
+  "RISCV64_verified" = toolchain_riscv64;
+  "X64_verified" = toolchain_x86_64;
 }
+
--- nix/sel4/default.nix
+++ nix/sel4/default.nix
@@ -1,69 +1,70 @@
 {
   nixpkgs,
   system ? builtins.currentSystem,
-  config ? "AARCH64_bcm2711_verified",
 }:
 let
   board_config = import ./config.nix { inherit nixpkgs; };
-  args = board_config.${config};
-in
-nixpkgs.stdenvNoCC.mkDerivation {
-  name = "sel4-kernel-${config}";
-
-  src = nixpkgs.fetchgit {
+  sel4_src = nixpkgs.fetchgit {
     url = "https://github.com/seL4/seL4.git";
-    rev = "f5e45a24531ad9ed28c56efb8346ab7398895fff";
-    hash = "sha256-cWMulMMe1+HFWokmroIZdpvM253P2gxbEWOBj3EDYHU=";
+    rev = "refs/tags/14.0.0";
+    hash = "sha256-kzRV3qIsfyIFoc2hT6l0cIyR6zLD4yHcPXCAbGAQGsk=";
   };
+in
+builtins.mapAttrs (
+  config_name: config_args:
+  nixpkgs.stdenvNoCC.mkDerivation {
+    name = "sel4-kernel-${config_name}";
+    src = sel4_src;
 
-  buildInputs = with nixpkgs; [
-    (python312.withPackages (
-      ps: with ps; [
-        pyyaml
-        pyfdt
-        jinja2
-        jsonschema
-        ply
-      ]
-    ))
-    args.bintools
-    args.cc
+    buildInputs = with nixpkgs; [
+      (python312.withPackages (
+        ps: with ps; [
+          pyyaml
+          pyfdt
+          jinja2
+          jsonschema
+          ply
+        ]
+      ))
+      config_args.bintools
+      config_args.cc
 
-    cmake
-    ninja
-    dtc
-    bash
-    libxml2
-  ];
+      cmake
+      ninja
+      dtc
+      bash
+      libxml2
+    ];
 
-  dontUseCmakeConfigure = true;
+    dontUseCmakeConfigure = true;
 
-  patchPhase = ''
-    patchShebangs .
-  '';
+    patchPhase = ''
+      patchShebangs .
+    '';
 
-  #phases = [ "unpackPhase" "patchPhase" "configurePhase" "buildPhase" ];
+    #phases = [ "unpackPhase" "patchPhase" "configurePhase" "buildPhase" ];
 
-  configurePhase = ''
-    mkdir _build
-    pushd _build
-    cmake \
-      -DCROSS_COMPILER_PREFIX=${args.toolchain} \
-      -DCMAKE_TOOLCHAIN_FILE=../gcc.cmake \
-      -DCMAKE_INSTALL_PREFIX=$out \
-      -G Ninja \
-      -C ../configs/${config}.cmake \
-      ../
-    popd
-  '';
+    configurePhase = ''
+      mkdir _build
+      pushd _build
+      cmake \
+        -DCROSS_COMPILER_PREFIX=${config_args.toolchain} \
+        -DCMAKE_TOOLCHAIN_FILE=../gcc.cmake \
+        -DCMAKE_INSTALL_PREFIX=$out \
+        -G Ninja \
+        -C ../configs/${config_name}.cmake \
+        ../
+      popd
+    '';
 
-  buildPhase = ''
-    pushd _build
-    ninja kernel.elf
-    popd
-  '';
+    buildPhase = ''
+      pushd _build
+      ninja kernel.elf
+      popd
+    '';
 
-  installPhase = ''
-    cmake --install _build/
-  '';
-}
+    installPhase = ''
+      cmake --install _build/
+    '';
+  }
+) board_config
